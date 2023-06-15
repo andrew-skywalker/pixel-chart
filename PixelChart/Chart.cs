e6214c9cffe2a23c5572043fc20b6c604d1c6826
Andrew M <andrew.myhalchuk@gmail.com>
2023-06-15 16:03:31 -0400

    add Chart with List<OHLC>

---
+++ PixelChart/Chart.cs
@@ -0,0 +1,36 @@
+﻿using PixelChart.Model;
+
+namespace PixelChart;
+
+internal class Chart
+{
+    public Dictionary<DateTime, int> DateToCoordDict = new();
+
+    List<OhlcCandle> _candles = new();
+    public List<OhlcCandle> Candles
+    {
+        get => _candles;
+        set
+        {
+            if (_candles != value)
+            {
+                DateToCoordDict.Clear();
+
+                foreach (var candle in value)
+                {
+                    if (!DateToCoordDict.ContainsKey(candle.Dt))
+                    {
+                        DateToCoordDict.Add(candle.Dt, candle.X);
+                    }
+                }
+
+                _candles = value;
+            }
+        }
+    }
+
+    public void Render(int width, int height)
+    {
+
+    }
+}
