fceda9bc78656c8e6ca87aa111e6aecc1e3e9160
Andrew M <andrew.myhalchuk@gmail.com>
2023-06-16 07:14:07 -0400

    utility methods in Chart

--- PixelChart/Chart.cs
+++ PixelChart/Chart.cs
@@ -8,8 +8,13 @@ internal class Chart
     //size variables
     public int chartAreaHeight = 300;
     public int chartAreaWidth = 1600;
+    public int LeftPadding { get; set; } = 1;
+    int candleWidth = 3;
+    int candleAreaWidth = 4;
 
     //data variables
+    decimal y_min;
+    decimal y_max;
     public Dictionary<DateTime, int> DateToCoordDict = new();
 
     List<OhlcCandle> _candles = new();
@@ -33,6 +38,30 @@ internal class Chart
                 _candles = value;
             }
         }
+    }
+
+    //utility methods
+    public void AutoScaleY()
+    {
+        y_max = Candles.Max(x => x.High);
+        y_min = Candles.Min(x => x.Low);
+    }
+
+    int CoordToPixelY(decimal coord)
+    {
+        decimal range = y_max - y_min;
+
+        decimal percentFromHigh = (y_max - coord) / range;
+
+        int pixel = (int)(chartAreaHeight * percentFromHigh);
+
+        return pixel;
+    }
+
+    public int CoordToPixelX(int x)
+    {
+        int pixel = LeftPadding + candleAreaWidth * x + 1;
+        return pixel;
     }
 
     //painting
