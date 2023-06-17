f36c038bb7279083b89b182b54568f2e9d53bccb
Andrew M <andrew.myhalchuk@gmail.com>
2023-06-17 12:07:03 -0400

    -

--- PixelChart/ColorScheme.cs
+++ PixelChart/ColorScheme.cs
@@ -12,8 +12,8 @@ internal class ColorScheme
     //current values
     public static Color colorBackground;
     public static Color colorBackgroundNonMh;
-    public static Color colorRedCandle;
     public static Color colorGreenCandle;
+    public static Color colorRedCandle;
     public static Color colorGrid;
     public static List<Color> labelColors = new() { };
     public static Color colorLables;
@@ -28,13 +28,13 @@ internal class ColorScheme
     //saved colors
     public static void SetColorScheme(string scheme)
     {
-        switch (scheme) 
+        switch (scheme)
         {
             case "light":
                 colorBackground = Color.FromArgb(250, 250, 250);
                 colorBackgroundNonMh = Color.FromArgb(235, 235, 235);
-                colorRedCandle = Color.FromArgb(110, 0, 0);
                 colorGreenCandle = Color.FromArgb(0, 110, 0);
+                colorRedCandle = Color.FromArgb(110, 0, 0);
                 colorGrid = Color.Black;
                 labelColors = new() { Color.FromArgb(46, 124, 237), Color.Red, Color.Green, Color.DarkGoldenrod, Color.Black };
                 colorLables = Color.Black;
@@ -45,8 +45,8 @@ internal class ColorScheme
             case "navy":
                 colorBackground = Color.Navy;
                 colorBackgroundNonMh = Color.DarkBlue;
-                colorRedCandle = Color.FromArgb(235, 0, 0);
                 colorGreenCandle = Color.FromArgb(0, 235, 0);
+                colorRedCandle = Color.FromArgb(235, 0, 0);
                 colorGrid = Color.DarkGray;
                 colorLables = Color.DarkGray;
                 dashPattern = new float[] { 1, 25 };
